96c69f4d3bc54dd9e98d109716c468828b5750ae
Habib Ullah Khan <ayubi.pakistani@gmail.com>
2019-04-15 19:35:21 +0500

    Migrations renewed

--- accounts/models.py
+++ accounts/models.py
@@ -10,7 +10,7 @@ class User(models.Model):
 
     first_name = models.CharField( max_length=30, blank=True)
     last_name = models.CharField( max_length=30, blank=True)
-    gender = models.CharField(choices=GENDER_OPTIONS,max_length=10, null=True, blank=True)
+    gender = models.CharField(choices=GENDER_OPTIONS, max_length=10, null=True, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
     is_live_user = models.BooleanField(default=True, verbose_name='Is_Live_User')
     is_maintenance_user = models.BooleanField(default=False, verbose_name='Maintenance_User')
@@ -34,27 +34,25 @@ class Mobile(models.Model):
     price = models.PositiveIntegerField()
     type = models.CharField(max_length=10)
     mobile_status = models.CharField(choices=status_choice, max_length=5)
+    IMEA_number = models.CharField(max_length=50, default="")
 
 
 class Customer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_customer")
-    date_to_return = models.DateField(default=None)
-
-    @property
-    def remaining_amount(self):
-        return self.transaction.sold_item.price - self.transaction.payed_amount
 
 
-class Sale(models.Model):
+class Transactions(models.Model):
     sold_item = models.ForeignKey(Mobile, on_delete=models.CASCADE, related_name="sale")
-    date_of_sale = models.DateField()
-    payed_amount = models.PositiveIntegerField()
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="transaction")
+    date_of_sale = models.DateField(default=None)
+    amount_payed = models.PositiveIntegerField(default=0)
+    insurer_one = models.ForeignKey(User, on_delete=models.CASCADE, related_name="first_insurer")
+    insurer_two = models.ForeignKey(User, on_delete=models.CASCADE, related_name="second_insurer")
+    next_installment_due = models.DateField(default=None, verbose_name="Next Installment Date")
+    previous_installment_payed = models.DateField(default=None, verbose_name="Previous Installment Payed")
+    number_of_installments_payed = models.PositiveIntegerField(default=0, verbose_name="Number Of Installments Payed")
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="customer")
 
-    @property
-    def payable_payment(self):
-        return self.sold_item.price
 
-    @property
-    def amount_owed(self):
-        return self.sold_item.price - self.payed_amount
+
+
+
