533cac277ab12004f19a5006264777be7552105f
Habib Ullah Khan <ayubi.pakistani@gmail.com>
2019-04-19 14:28:14 +0500

    static files added

--- accounting/settings.py
+++ accounting/settings.py
@@ -126,5 +126,7 @@ REST_FRAMEWORK = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
