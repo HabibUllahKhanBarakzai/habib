8c157ed238a9442e1a35b2fdf1eb880153ad29af
habib ullah khan home <ayubi.pakistani@gmail.com>
2019-04-21 17:36:31 +0500

    accounts module reconfigured

--- accounts/models.py
+++ accounts/models.py
@@ -13,9 +13,9 @@ class Our_User(models.Model):
     father_name = models.CharField(max_length=60, blank=True)
     gender = models.CharField(choices=GENDER_OPTIONS, max_length=10, null=True, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
-    is_live_user = models.BooleanField(default=True, verbose_name='Is_Live_User')
-    is_maintenance_user = models.BooleanField(default=False, verbose_name='Maintenance_User')
-    address = models.CharField(max_length=250)
+    is_live_user = models.BooleanField(default=True, verbose_name='Is Live User')
+    house_address = models.CharField(max_length=250)
+    business_address = models.CharField(max_length=250, default="")
     CNIC_number = models.CharField(blank=False, unique=True, max_length=15)
 
 
@@ -42,8 +42,6 @@ class Mobile(models.Model):
 
 class Customer(models.Model):
     user = models.OneToOneField(Our_User, on_delete=models.CASCADE, related_name="user_customer")
-    installments_payed = ArrayField(JSONField(), verbose_name="History of installments payed",
-                                    default=list)
 
 
 class Transactions(models.Model):
@@ -56,9 +54,10 @@ class Transactions(models.Model):
     insurer_two = models.ForeignKey(
         Our_User, on_delete=models.CASCADE, related_name="second_insurer")
     next_installment_due = models.DateField(default=None, verbose_name="Next Installment Date")
-    previous_installment_payed = models.DateField(
-        default=None, verbose_name="Previous Installment Payed")
     number_of_installments_payed = models.PositiveIntegerField(
         default=0, verbose_name="Number Of Installments Payed")
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, related_name="customer")
     is_return = models.BooleanField(default=False)
+    installments_payed = ArrayField(JSONField(), verbose_name="History of installments payed",
+                                    default=list)
+    installment_amount = models.PositiveIntegerField()
